ff8f1a285f215a997490e687bf264b24d01eaa94
Solal Chabasseur <solal.chabasseur@epfedu.fr>
2022-11-08 15:01:49 +0100

    Création du projet Heritage

---
+++ TP3 Héritage, classe abstraite et interface/src/tp3/héritage/classe/abstraite/et/pkginterface/TP3HéritageClasseAbstraiteEtInterface.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3.héritage.classe.abstraite.et.pkginterface;
+
+/**
+ *
+ * @author solal
+ */
+public class TP3HéritageClasseAbstraiteEtInterface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
