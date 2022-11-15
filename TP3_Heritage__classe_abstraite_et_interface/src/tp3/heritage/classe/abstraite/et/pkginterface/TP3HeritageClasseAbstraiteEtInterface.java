cf060269653c019166c1fe4a62e497141ddff047
Solal Chabasseur <solal.chabasseur@epfedu.fr>
2022-11-15 12:48:36 +0100

    Partie 3 quasi finie

--- TP3_Heritage__classe_abstraite_et_interface/src/tp3/heritage/classe/abstraite/et/pkginterface/TP3HeritageClasseAbstraiteEtInterface.java
+++ TP3_Heritage__classe_abstraite_et_interface/src/tp3/heritage/classe/abstraite/et/pkginterface/TP3HeritageClasseAbstraiteEtInterface.java
@@ -4,6 +4,12 @@
  */
 package tp3.heritage.classe.abstraite.et.pkginterface;
 
+import Armes.Baton;
+import Armes.Armes;
+import Armes.Epee;
+import Personnage.Personnage;
+import Personnage.Magicien;
+import Personnage.Guerrier;
 import java.util.ArrayList;
 
 /**
@@ -22,20 +28,54 @@ public class TP3HeritageClasseAbstraiteEtInterface {
         Baton baton1 = new Baton("Chêne",4,5);
         Baton baton2 = new Baton ("Charme", 5, 6);
         
-        System.out.println(epee1);
-        System.out.println(baton1);
+        ArrayList<Armes> tabArme = new ArrayList<Armes>();
+        tabArme.add(epee1);
+        tabArme.add(epee2);
+        tabArme.add(baton1);
+        tabArme.add(baton2);
         
+        for (int i=0; i<4;i++){
+            System.out.print(tabArme.get(i));
+        }
+        System.out.println();
         
-        ArrayList<Armes> tab = new ArrayList<Armes>();
-        tab.add(epee1);
-        tab.add(epee2);
-        tab.add(baton1);
-        tab.add(baton2);
+        Magicien mage1 = new Magicien ("Gandalf", 65, "confirme");
+        Magicien mage2 = new Magicien ("Garcimore", 44, "novice");
         
-        System.out.print(tab.get(0));
-        System.out.print(tab.get(1));
-        System.out.print(tab.get(2));
-        System.out.print(tab.get(3));
+        Guerrier guerr1 = new Guerrier("Conan",78,"a pied");
+        Guerrier guerr2 = new Guerrier("Lannister",45,"cheval");
         
+        ArrayList<Personnage> tabPerso = new ArrayList<Personnage>();
+        tabPerso.add(mage1);
+        tabPerso.add(mage2);
+        tabPerso.add(guerr1);
+        tabPerso.add(guerr2);
+        
+        for (int i=0; i<4;i++){
+            System.out.print(tabPerso.get(i));
+        }
+        
+        Magicien mage3 = new Magicien ("Abdellah Makhchan", 99, "confirme");
+        Guerrier guerr3 = new Guerrier("Jerome Taillefer",90,"cheval");
+        
+        Epee epee3 = new Epee ("Bazouzou", 69, 96);
+        Epee epee4 = new Epee ("Poutre de Bamako", 99, 2);
+        Epee epee5 = new Epee ("Braquemar", 80, 60);
+        
+        Baton baton3 = new Baton("Boulot",34,9);
+        Baton baton4 = new Baton ("Tige de l'infie", 78, 6);
+        Baton baton5 = new Baton ("Le Nawfel", 1, 19);
+        
+       guerr3.ajouterArme(epee5);
+       guerr3.ajouterArme(epee4);
+       guerr3.ajouterArme(baton3);
+       guerr3.EquipArme("Braquemar");
+       
+       mage3.ajouterArme(baton4);
+       mage3.ajouterArme(baton5);
+       mage3.ajouterArme(epee3);
+       mage3.EquipArme("Le Nawfel");
+       
+      
     }
 }
