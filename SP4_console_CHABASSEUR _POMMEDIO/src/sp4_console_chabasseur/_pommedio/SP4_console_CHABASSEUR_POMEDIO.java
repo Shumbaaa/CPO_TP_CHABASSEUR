c5f41fbea8ad2398c13b1ce0c73283f36165c444
Solal Chabasseur <solal.chabasseur@epfedu.fr>
2022-11-18 10:40:09 +0100

    Début du Super Puissance 4, classe Jeton terminé.

---
+++ SP4_console_CHABASSEUR _POMMEDIO/src/sp4_console_chabasseur/_pommedio/SP4_console_CHABASSEUR_POMEDIO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sp4_console_chabasseur._pommedio;
+
+/**
+ *
+ * @author solal
+ */
+public class SP4_console_CHABASSEUR_POMEDIO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Jeton jeton1 = new Jeton("jaune");
+        System.out.println(jeton1);
+    }
+    
+}
