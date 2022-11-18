5c679d86d13b312fa0840e5f3e658207e30cbbbc
Solal Chabasseur <solal.chabasseur@epfedu.fr>
2022-11-18 11:42:15 +0100

    Finalisation de la classe CelluleDeGrille. Création de la classe PlateauDeJeu.

--- SP4_console_CHABASSEUR _POMMEDIO/src/sp4_console_chabasseur/_pommedio/CelluleDeGrille.java
+++ SP4_console_CHABASSEUR _POMMEDIO/src/sp4_console_chabasseur/_pommedio/CelluleDeGrille.java
@@ -13,12 +13,12 @@ public class CelluleDeGrille {
     private boolean avoirTrouNoir;
     private boolean avoirDesintegrateur;
     
-    public CelluleDeGrille(){
+    public CelluleDeGrille(){ //initialise une cellule de grille
         jetonCourant=null;
         avoirTrouNoir=false;
         avoirDesintegrateur=false;
     }
-    public boolean presenceJeton(){
+    public boolean presenceJeton(){ //indique si une cellule possède un jeton ou non
         if (jetonCourant==null){
             return false;
         }else{
@@ -26,11 +26,11 @@ public class CelluleDeGrille {
         }
     }
 
-    public void affecterJeton(String unJeton){
+    public void affecterJeton(String unJeton){ //affecte un jeton dans une cellule
         jetonCourant=unJeton;
     }
 
-    public String lireCouleurDuJeton() {
+    public String lireCouleurDuJeton() { //permet de renvoyer la couleur du jeton d'une cellule ou "vide" si il n'y a pas de jeton
         if (jetonCourant==null){
             return "vide";
         }else{
@@ -38,10 +38,47 @@ public class CelluleDeGrille {
         }
     }
     
-    public void placerTrouNoir(){
+    public void placerTrouNoir(){ //permet de placer un trou noir dans une cellule
         avoirTrouNoir=true;
     }
-    public void supprimerTrouNoir(){  
+    public void supprimerTrouNoir(){  //permet de supprimer un trou noir dans une cellule
         avoirTrouNoir=false;
     }
+    
+    public boolean presenceTrouNoir(){ //permet de savoir si une cellule possède un trou noir ou non
+        if(avoirTrouNoir==true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+   
+    public String recupererJeton(){ 
+        String jetonRecuperer=jetonCourant; //on recupere le jeton de la cellule que l'on met dans une variable temoporaire
+        jetonCourant=null; //on donne la valeur null au jeton que l'on vient de récuperer
+        return jetonRecuperer; //on retourne la valeur du jeton que l'on a récuperer
+    }
+    public void supprimerJeton(){
+        jetonCourant=null; //suppression du jeton, on ne le récupère pas
+    }
+    
+    public boolean presenceDesintegrateur(){ 
+        if (avoirDesintegrateur==true){ //renvoi true s'il y a un desintegrateur
+            return true;
+        }else{
+            return false; //renvoi false s'il n'y a pas un desintegrateur
+        }
+    }
+    
+    public void placerDesintegrateur(){
+        avoirDesintegrateur=true;  //place un desintegrateur dans la cellule
+    }
+    public void supprimerDesintegrateur(){
+        avoirDesintegrateur=false;  //supprime le desintegrateur de la cellule
+    }
+    public void activerTrouNoir(){ //supprime le jeton et le trou noir
+        supprimerJeton();
+        supprimerTrouNoir();
+    }
+    
 }
